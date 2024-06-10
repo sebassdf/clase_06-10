b0a22010a7b9a80e2af8ae6eb0b6cc4af5aadc8a
unknown <SSDD@PNAWSA06LC1026.duoc.acad>
2024-06-10 16:10:34 -0400

    segundo commit

--- alumnos/urls.py
+++ alumnos/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [    
-    path('index', views.index , name="index")
-]
+urlpatterns = [
+    path('index', views.index , name="index"),
+    path('crud', views.crud , name="crud"),
+    path('alumnosAdd', views.alumnosAdd , name="alumnosAdd"),
+    path('alumnos_del/<str:pk', views.alumnos_del , name="alumnos_del"),
+    path('alumnos_findEdit/<str:pk', views.alumnos_findEdit , name="alumnos_findEdit")
+]    
+ 
+
--- alumnos/views.py
+++ alumnos/views.py
@@ -6,3 +6,17 @@ def index(request):
     alumnos = Alumno.objects.all()
     context={"alumnos":alumnos}
     return render(request,"alumnos/index.html",context)
+
+def crud(request):
+    alumnos = Alumno.objects.all()
+    context={"alumnos":alumnos}
+    return render(request,"alumnos/alumnos_list.html",context)
+
+def alumnosAdd(request):
+    return "hola"
+
+def alumnos_del(request,pk):
+    return "hola"
+
+def alumnos_findEdit(request,pk):
+    return "Hola"
